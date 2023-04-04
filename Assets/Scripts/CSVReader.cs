88ccf55272e2d4fce61f9d0b5759ab6c9a533a86
BauerPhilipp <fam.bauer@live.at>
2023-04-04 20:50:10 +0200

    Datenbank Grundaufbau funktionstüchtig

---
+++ Assets/Scripts/CSVReader.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CSVReader : MonoBehaviour
+{
+
+    [SerializeField] TextAsset csvDatei;
+    int spaltenAnzahl = 0;
+    int reihenAnzahl = 0;
+    List<Kunde> kundenListe = new List<Kunde>();
+
+    private void Start()
+    {
+        string[] reihe = csvDatei.text.Split("\n", StringSplitOptions.None);
+        reihenAnzahl = reihe.Length ;
+        string[] data = csvDatei.text.Split(new string[] { ";", "\n" }, StringSplitOptions.None);
+        spaltenAnzahl = data.Length / reihenAnzahl - 1;
+        //Debug.Log("Einträge in data: " + data.Length);
+        //Debug.Log("Reihen: " + reihenAnzahl);
+        //Debug.Log("Spalten -1: " + spaltenAnzahl);
+
+        //foreach (string s in data)
+        //{
+        //    Debug.Log(s);
+        //}
+
+        int offset = 0;
+        for (int i = 0; i < reihenAnzahl; i++)
+        {
+            Kunde kunde = new Kunde();
+            kunde.Kundenbezeichnung = data[i + offset];
+            kunde.Name = data[i + 1 + offset];
+            kunde.Firma = data[i + 2 + offset];
+            kunde.Straße = data[i + 3 + offset];
+            kunde.Postleitzahl = data[i + 4 + offset];
+            kunde.Land = data[i + 5 + offset];
+
+            //Debug.Log(data[i + offset]);
+            offset += 6;
+
+            kundenListe.Add(kunde);
+        }
+        //AusgabeKundenliste();
+    }
+
+    //void AusgabeKundenliste()
+    //{
+    //    foreach (Kunde kunde in kundenListe)
+    //    {
+    //        Debug.Log("Kunde: " + kunde.Kundenbezeichnung + ", "
+    //                + "Name: " + kunde.Name + ", "
+    //                + "Firma: " + kunde.Firma + ", "
+    //                + "Straße: " + kunde.Straße + ", "
+    //                + "Postleitzahl: " + kunde.Postleitzahl + ", "
+    //                + "Land: " + kunde.Land + ", ");
+    //    }
+    //}
+
+}
---
+++ Assets/Scripts/Kunde.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+public struct Kunde
+{
+    public string Kundenbezeichnung { get; set; }
+    public string Name { get; set; }
+    public string Firma { get;  set; }
+    public string Straﬂe { get; set; }
+    public string Postleitzahl { get; set; }
+    public string Land { get;  set; }
+
+    public Kunde(string Kundenbezeichnung, string Name, string Firma, string Straﬂe, string Postleitzahl, string Land)
+    {
+        this.Kundenbezeichnung = Kundenbezeichnung;
+        this.Name = Name;
+        this.Firma = Firma;
+        this.Straﬂe = Straﬂe;
+        this.Postleitzahl = Postleitzahl;
+        this.Land = Land;       
+    }
+}
